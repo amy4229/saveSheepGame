57770bac0f79a1936cfaf3d3e885cae4bc838d91
amykim <amykim4229@gmail.com>
2022-05-26 17:23:53 +0900

    220526 Update  Modify component class name of HTML, CSS BEM by naming rules

--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,72 @@
+'use strict';
 const GUN_SOUND = "/sounds/180961__kleeb__gunshots.wav";
 const BGM = "/sounds/식물 소개.mp3";
 const WIN_SOUND = "/sounds/425663__camo1018__clapping.mp3";
 const LOSE_SOUND = "/sounds/253174__suntemple__retro-you-lose-sfx.wav";
 
-const btn_comp = document.querySelector("button");
-const sun_comp = document.querySelector(".sun-image");
-const main_comp = document.querySelector("main");
-const message_comp = document.querySelector(".message");
-const timeLimit_comp  = document.querySelector(".time-limit");
-const wolfRemain_comp  = document.querySelector(".remain-wolves");
+const btn_comp = document.querySelector(".game__header__btn");
+const sun_comp = document.querySelector(".game__header__sun__img");
+const farm_comp = document.querySelector(".game__farm");
+const message_comp = document.querySelector(".game__display__message");
+const timeLimit_comp  = document.querySelector(".game__display__timer");
+const wolfRemain_comp  = document.querySelector(".game__display__remain_wolves");
 
 const GAME_WIDTH = 800;
 const SUN_WIDTH = 150;
-const TIME_LIMIT_SEC =10;
+const TIME_LIMIT_SEC =60;
 const NUMBER_OF_SHEEP = 5;
 const NUMBER_OF_WOLVES =5;
 const WIDTH_OF_ANIMAL = 100;
 const HEIGHT_OF_ANIMAL = 70;
 
-const CLASS_END = "end"
-const CLASS_SHEEP = "sheep"
-const CLASS_WOLF = "wolf"
-const CLASS_DIE = "die"
+const CLASS_END = "game__header__btn--end";
 
 const MSG_GAME_OVER_SHEEP_KILL = "당신은 양을 죽여서 해고되었습니다."
 const MSG_GAME_OVER_TIME_OUT = "시간이 초과되었습니다."
 const MSG_GAME_OVER_USER_REQUEST = "요청에 따라 게임을 종료하였습니다."
 const MSG_GAME_OVER_WIN = "늑대를 모두 없앴습니다. 축하드립니다."
 
+const Animal = Object.freeze({
+    sheep : {
+        name : "sheep"
+        ,class :"game__farm__sheep"
+    },
+    wolf : {
+        name : "wolf"
+        ,class :"game__farm__wolf"
+    },
+    deadWolf : {
+        name : "dead wolf"
+        ,class : "game__farm__wolf--die"
+    }
+});
+
 let sunMove_ani = null;
-let clock_intersval = null;
+let clock_interval = null;
 let timeout = null;
 let killed_wolves = 0;
 
-let bgm = new Audio(BGM);
-let winSound = new Audio(WIN_SOUND);
-let loseSound = new Audio(LOSE_SOUND);
+const bgm = new Audio(BGM);
+const winSound = new Audio(WIN_SOUND);
+const loseSound = new Audio(LOSE_SOUND);
+const shotSound = new Audio(GUN_SOUND);
 
 btn_comp.addEventListener("click", onButtonClick);
 
-main_comp.addEventListener("mousedown", ()=>{
-    const shotSound = new Audio(GUN_SOUND);
+farm_comp.addEventListener("mousedown", (e)=>{
+    shotSound.currentTime = 0;
     shotSound.play();
+    const target = e.target;
+    if(target.matches(`.${Animal.wolf.class}`)){
+        shotWolf(target);
+    }else if(target.matches(`.${Animal.sheep.class}`)){
+        shotSheep();
+    }
 })
 
 function onButtonClick(){
     const cmd = btn_comp.className;
+    console.log(cmd);
     btn_comp.classList.toggle(CLASS_END);
     if(cmd !== CLASS_END){
         console.log("start click")
@@ -57,7 +78,7 @@ function onButtonClick(){
 }
 
 function start (){
-    console.log("start Start" );
+    console.log("start" );
     if(winSound)winSound.pause();
     if(loseSound)loseSound.pause();
     bgm.play();
@@ -69,46 +90,50 @@ function start (){
     tictok();
     printSheep(NUMBER_OF_SHEEP);
     printWolf(NUMBER_OF_WOLVES);
-    timeout = setTimeout(()=>lose(MSG_GAME_OVER_TIME_OUT), 10000);
+    timeout = setTimeout(()=>lose(MSG_GAME_OVER_TIME_OUT), TIME_LIMIT_SEC*1000);
 }
 
 
 
 function printSheep(howMany){
     for(let i = 0; i < howMany; i++ ){
-        const sheepKill =()=>lose(MSG_GAME_OVER_SHEEP_KILL);
-        makeAnimal(CLASS_SHEEP,sheepKill);
+        makeAnimal(Animal.sheep);
     }
+}
+
+function shotSheep() {
+    lose(MSG_GAME_OVER_SHEEP_KILL);
 }
 
 function printWolf(howMany){
     for(let i = 0; i < howMany; i++ ){
-        makeAnimal(CLASS_WOLF, shotWolf)
+        makeAnimal(Animal.wolf, shotWolf)
     }
 }
 
-function makeAnimal (animalClass, callback) {
-    const animal = document.createElement("div");
-    animal.classList.add(animalClass);
-    animal.style.left = getRandomX(); 
-    animal.style.top = getRandomY();
-    main_comp.appendChild(animal);
-    animal.addEventListener("click", callback);
+function makeAnimal (animal) {
+    const animal_comp = document.createElement("img");
+    animal_comp.classList.add(animal.class);
+    animal_comp.setAttribute("alt",animal.name);
+    animal_comp.style.left = getRandomX(); 
+    animal_comp.style.top = getRandomY();
+    farm_comp.appendChild(animal_comp);
 }
 
 function getRandomX (){
-    const max_x = main_comp.clientWidth-WIDTH_OF_ANIMAL;
+    const max_x = farm_comp.clientWidth-WIDTH_OF_ANIMAL;
     return Math.floor(Math.random()* max_x)+"px";
 }
 
 function getRandomY (){
-    const max_y = main_comp.clientHeight-HEIGHT_OF_ANIMAL;
+    const max_y = farm_comp.clientHeight-HEIGHT_OF_ANIMAL;
     return Math.floor(Math.random()* max_y)+"px";
 }
 
-function shotWolf(e){
-    e.target.classList.add(CLASS_DIE);
-    e.target.removeEventListener("click",shotWolf);
+function shotWolf(wolf){
+    wolf.classList.add(Animal.deadWolf.class);
+    wolf.classList.remove(Animal.wolf.class);
+    wolf.setAttribute("alt", Animal.deadWolf.name);
     killed_wolves++;
     wolfRemain_comp.textContent = `남은 늑대: ${NUMBER_OF_WOLVES - killed_wolves}마리`;
     if(killed_wolves == NUMBER_OF_WOLVES){
@@ -121,7 +146,7 @@ function moveSun(){
     sunMove_ani = sun_comp.animate([
         {"transform" : "translateX("+(GAME_WIDTH-SUN_WIDTH)+"px)"}
     ],{
-        duration: 10000,
+        duration : TIME_LIMIT_SEC * 1000,
         iterations: 1,
       });
 
@@ -129,18 +154,16 @@ function moveSun(){
 
 function tictok(){
     let timeRemain = TIME_LIMIT_SEC-1;
-    clock = setInterval(function(){
+    clock_interval = setInterval(function(){
         timeLimit_comp.textContent =`남은 시간: ${timeRemain}초` ;
         timeRemain--;
     },1000)
 }
 function win (){
-    //winSound = ;
     winSound.play();
     end(MSG_GAME_OVER_WIN);
 }
 function lose (msg) {
-    //loseSound = new Audio(LOSE_SOUND);
     loseSound.play();
     end(msg);
 } 
@@ -149,9 +172,9 @@ function end (msg){
     bgm.pause();
     btn_comp.classList.remove("end");
     console.log("endStart");
-    main_comp.innerHTML = "";
+    farm_comp.innerHTML = "";
     stopSun(sunMove_ani);
-    clearInterval(clock);
+    clearInterval(clock_interval);
     clearTimeout(timeout);
     message_comp.textContent = msg;
 }
