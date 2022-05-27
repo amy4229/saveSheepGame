28a0aec13abca2bda9b67137ca13b8c49832d79c
amy <amykim4229@gmail.com>
2022-05-27 17:46:52 +0900

    220527 Modify timelimit
    60s => 10s

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ import GameBuilder from "./game.js"
 
 //const GAME_WIDTH = 800;
 //const SUN_WIDTH = 150;
-const TIME_LIMIT_SEC = 60;
+const TIME_LIMIT_SEC = 10;
 const NUMBER_OF_SHEEP = 5;
 const NUMBER_OF_WOLVES = 5;
 
